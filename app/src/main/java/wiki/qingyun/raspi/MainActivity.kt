c2a7cd89efeb96ad54db08109cc60e187c2e8799
QingYun <175226677@qq.com>
2023-12-03 22:42:02 +0800

    jump

--- app/src/main/java/wiki/qingyun/raspi/MainActivity.kt
+++ app/src/main/java/wiki/qingyun/raspi/MainActivity.kt
@@ -1,29 +1,17 @@
 package wiki.qingyun.raspi
-
-import android.app.AlertDialog
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveableStateHolder
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.window.Dialog
-import wiki.qingyun.raspi.demo.Demo
 import wiki.qingyun.raspi.presentation.Login
+import wiki.qingyun.raspi.presentation.Main
 import wiki.qingyun.raspi.ui.theme.RaspiTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,9 +19,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             RaspiTheme {
+                var layout by remember {
+                    mutableStateOf("Login")
+                }
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Login()
+                    when(layout) {
+                        "Login" -> {
+                            Login { login ->
+                                if(login){
+                                    layout = "Main"
+                                }
+                            }
+                        }
+                        "Main" -> {
+                            Main()
+                        }
+                    }
                 }
             }
         }
--- app/src/main/java/wiki/qingyun/raspi/presentation/Login.kt
+++ app/src/main/java/wiki/qingyun/raspi/presentation/Login.kt
@@ -1,16 +1,12 @@
 package wiki.qingyun.raspi.presentation
 
-import android.graphics.Paint.Style
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -21,11 +17,9 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -34,15 +28,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.draw.paint
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -54,8 +43,16 @@ import androidx.compose.ui.unit.sp
 import wiki.qingyun.raspi.R
 import wiki.qingyun.raspi.ui.theme.RaspiTheme
 
+data class Info (
+    val username : String,
+    val password : String
+)
 @Composable
-fun Login() {
+fun Login(onLogin : (Boolean) -> Unit) {
+    var user by remember {
+        mutableStateOf(Info("",""))
+    }
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -69,8 +66,15 @@ fun Login() {
             ),
     ) {
         LoginTitle()
-        LoginContent()
+        LoginContent{username, password ->
+            user = Info(username, password)
+            onLogin(check(user))
+        }
     }
+}
+
+fun check(info : Info) : Boolean {
+    return true
 }
 
 @Composable
@@ -92,7 +96,13 @@ fun LoginTitle() {
 }
 
 @Composable
-fun LoginContent() {
+fun LoginContent(onLogin: (String, String) -> Unit) {
+    var username by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -104,15 +114,20 @@ fun LoginContent() {
     ) {
         Column(
         ) {
-            LoginInputField()
-            LoginButton()
+            LoginInputField(
+                username = {username = it},
+                password = {password = it})
+            LoginButton(username, password, onLogin)
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginInputField() {
+fun LoginInputField(
+    username: (String) -> Unit,
+    password: (String) -> Unit
+) {
     var username by remember {
         mutableStateOf("")
     }
@@ -136,7 +151,9 @@ fun LoginInputField() {
                 containerColor = Color(0xFFD8E9E4)
             ),
             value = username,
-            onValueChange = { username = it },
+            onValueChange = {
+                username = it
+                username(it)},
             placeholder = { Text(text = "请输入账号")},
             label = { Text(text = "账号") },
             leadingIcon = { Icon(Icons.Filled.AccountCircle, contentDescription = "") }
@@ -149,7 +166,9 @@ fun LoginInputField() {
                 containerColor = Color(0xFFD8E9E4)
             ),
             value = password,
-            onValueChange = { password = it },
+            onValueChange = {
+                password = it
+                password(it)},
             placeholder = { Text(text = "请输入密码")},
             label = { Text(text = "密码") },
             visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
@@ -168,7 +187,7 @@ fun LoginInputField() {
 }
 
 @Composable
-fun LoginButton() {
+fun LoginButton(username: String, password: String, onLogin: (String, String) -> Unit) {
     Column(
         modifier = Modifier
             .fillMaxWidth(),
@@ -181,7 +200,7 @@ fun LoginButton() {
                 text = "忘记密码?",
                 textDecoration = TextDecoration.Underline
             ) },
-            onClick = { /*TODO*/ }
+            onClick = {  }
         )
     }
     Column(
@@ -195,7 +214,7 @@ fun LoginButton() {
                 .height(65.dp)
                 .padding(start = 30.dp, end = 30.dp, top = 10.dp),
             shape = RoundedCornerShape(10.dp),
-            onClick = { /*TODO*/ }
+            onClick = { onLogin(username, password) }
         ) {
             Text(
                 text = "登 录",
@@ -210,6 +229,6 @@ fun LoginButton() {
 @Composable
 fun LoginPreview() {
     RaspiTheme {
-        Login()
+//        Login(fun():Unit{})
     }
 }
